751ef0174590174bb9b8676ef626d68b6a7a4eab
Sydney Sanders <syd@Sydneys-MacBook-Air.local>
2022-10-15 15:55:31 -0400

    this isupdated

---
+++ index.js
@@ -0,0 +1,6 @@
+const page_loader = document.getElementById("page-loader")
+setTimeout(function() { 
+let page_gif_loader = document.getElementById("gif");
+    page_gif_loader.classList.add("fade-out");
+},5200)
+
